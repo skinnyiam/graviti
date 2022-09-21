aa46e3330ee2b88ccbad90709238fc558294b5fd
Saurabh <ss6408367@gmail.com>
2022-09-21 19:29:15 +0530

    implemented route function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  Marker,
+  Autocomplete,
+  DirectionsRenderer,
+} from "@react-google-maps/api";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const containerStyle = {
+  width: "1000px",
+  height: "1000px",
+};
+
+const center = { lat: 28.351839, lng: 79.409561 }
+
+function MyComponent() {
+
+  const [directions, setDirections] = useState("")
+  const [distance, setDistance] = useState('')
+   /** @type React.MutableRefObject<HTMLInputElement> */
+   const originRef = useRef()
+   /** @type React.MutableRefObject<HTMLInputElement> */
+   const destiantionRef = useRef()
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyAolXVBph__8LXk-JukgnxDUI4LPDQAsxQ",
+    libraries: ["places"],
+  });
+
+  async function calculateRoute(event) {
+    event.preventDefault();
+    // if (originRef.current.value === '' || destiantionRef.current.value === '') {
+    //   return
+    // }
+    // eslint-disable-next-line no-undef
+    const directionsService = new google.maps.DirectionsService()
+    const results = await directionsService.route({
+      origin: originRef.current.value,
+      destination: destiantionRef.current.value,
+      // eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.DRIVING,
+    })
+    setDirections(results)
+    setDistance(results.routes[0].legs[0].distance.text)
+    console.log(results.routes[0].legs[0].distance.text)
+    
+  }
+
+  return isLoaded ? (
+    <>
+    
+      <Autocomplete>
+        <input type="text" placeholder="source" ref={originRef}/>
+      </Autocomplete>
+      <Autocomplete>
+        <input type="text" placeholder="destination"  ref={destiantionRef}/>
+      </Autocomplete>
+      <button onClick={calculateRoute}>Submit</button>
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={7}>
+        <Marker />
+        {directions && (
+            <DirectionsRenderer directions={directions} />
+          )}
+        <></>
+      </GoogleMap>
+    </>
+  ) : (
+    <></>
   );
 }
 
-export default App;
+export default React.memo(MyComponent);
