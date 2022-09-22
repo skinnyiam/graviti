7d5569ca55dc8dcbff1a3c315a34d1515872e9a6
Saurabh <ss6408367@gmail.com>
2022-09-22 16:53:20 +0530

    improved the details of graviti

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import Navbar from "./components/navbar";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Navbar />
     <App />
   </React.StrictMode>
 );
